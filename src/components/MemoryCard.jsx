796465d7b59964af310cf869ba61844f01727cbd
Utku Cikmaz <utkucikmaz@gmail.com>
2023-09-26 16:00:56 +0200

    favorite button for memory cards

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -1,8 +1,28 @@
-import React, { useContext } from "react";
-import { NavLink } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 import { AuthContext } from "../../context/auth.context";
 
 export default function MemoryCard(props) {
+    const [isClicked, setIsClicked] = useState(false);
+    const { eventId } = useParams();
+
+    const handleClick = () => {
+        setIsClicked(!isClicked);
+        onClickFavButton();
+    };
+
+    const onClickFavButton = (e) => {
+        const requestBody = { favorite: true };
+        axios.put(
+            `${import.meta.env.VITE_API_URL}/api/events/${eventId}`,
+            requestBody
+        );
+        // .then((response) => {
+        //     navigate(`/events/${eventId}`);
+        // });
+    };
+
     let createdDate = new Date(props.memory.createdAt);
     let timeDiff = Date.now() - createdDate;
     let friendlyTimeStamp;
@@ -74,10 +94,39 @@ export default function MemoryCard(props) {
                                 className="fas fa-retweet"
                                 data-testid="retweet-icon"
                             ></i>
-                            <i
-                                className="far fa-heart"
-                                data-testid="heart-icon"
-                            ></i>
+                            <button
+                                type="button"
+                                id="FavTestId"
+                                onClick={handleClick}
+                                data-cy="FavTest"
+                            >
+                                {props.memory.favorite || isClicked ? (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="30"
+                                        height="30"
+                                        fill="red"
+                                        className="bi bi-heart-fill"
+                                        viewBox="0 0 16 16"
+                                    >
+                                        <path
+                                            fillRule="evenodd"
+                                            d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
+                                        />
+                                    </svg>
+                                ) : (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="30"
+                                        height="30"
+                                        fill="red"
+                                        className="bi bi-heart"
+                                        viewBox="0 0 16 16"
+                                    >
+                                        <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+                                    </svg>
+                                )}
+                            </button>
                             <i
                                 className="fas fa-share"
                                 data-testid="share-icon"
