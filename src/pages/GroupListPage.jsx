455962434b2fb3b50a72a940d3aa66b398b69727
Utku Cikmaz <utkucikmaz@gmail.com>
2023-10-05 20:57:58 +0200

    adding signup and login buttons in my group page

--- src/pages/GroupListPage.jsx
+++ src/pages/GroupListPage.jsx
@@ -3,6 +3,7 @@ import { AuthContext } from "../../context/auth.context";
 import axios from "axios";
 import AddGroup from "../components/AddGroup";
 import GroupCard from "../components/GroupCard";
+import { Link } from "react-router-dom";
 
 function GroupListPage() {
     const [groups, setGroups] = useState([]);
@@ -43,6 +44,18 @@ function GroupListPage() {
             ) : (
                 <>
                     <p>Please Login to see the groups</p>
+                    <div>
+                        <Link to="/signup">
+                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4">
+                                Sign up
+                            </button>
+                        </Link>
+                        <Link to="/login">
+                            <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded ml-4">
+                                Login
+                            </button>
+                        </Link>
+                    </div>
                 </>
             )}
         </div>
@@ -50,21 +63,3 @@ function GroupListPage() {
 }
 
 export default GroupListPage;
-
-//     return (
-//         <div className="flex flex-col w-full justify-center items-center gap-10 p-2">
-//             <AddGroup refreshGroups={getAllGroups} />
-//             <div className="flex flex-wrap max-w-screen-xl gap-10">
-//                 {groups.map((group) => {
-//                     return group ? (
-//                         <GroupCard group={group} key={group._id}></GroupCard>
-//                     ) : (
-//                         <p>Loading....</p>
-//                     );
-//                 })}
-//             </div>
-//         </div>
-//     );
-// }
-
-// export default GroupListPage;
